b20f44ff8b6dadbed44a764639b08a87423007f9
Adopcalipt <79842781+Adopcalipt@users.noreply.github.com>
2022-09-27 15:52:54 +0100

    Folder in Folder error

--- New Street Phone Missions/New Street Phone Missions/Classes/Tattoo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace New_Street_Phone_Missions.Classes
-{
-    public class Tattoo
-    {
-        public string BaseName { get; set; }           
-        public string TatName { get; set; }             
-        public string Name { get; set; }    
-    }
-}
